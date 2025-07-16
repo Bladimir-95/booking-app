ccc9f23c576cb3b597496db411053803c611cd99
ferch <ferch123@Gmail.com>
2025-07-15 23:04:17 -0600

    Vista & funcionamiento del home

--- controller/AlojamientoController.php
+++ controller/AlojamientoController.php
@@ -1,12 +1,13 @@
-<?php 
-    require_once './config/database.php';
-    require_once './models/Alojamiento.php';
+<?php
+require_once './config/database.php';
+require_once './models/alojamiento.php';
 
-    class AlojamientoController{
-        public $alojamiento;
-        public $db;
 
-        public function __construct()
+class AlumnoController {
+    public $db;
+    public $alojamiento;
+
+    public function __construct()
         {
             $database = new Database();
             $this->db = $database->getConnection();
@@ -14,27 +15,41 @@
         }
 
         public function read(){
-            // Logica para Leer alojamientos
+            // Logica para Leer alumnos
             $sentence = $this->alojamiento->read();
-            $alojamiento = $sentence->fetchAll(PDO::FETCH_ASSOC);
+            $alojaminetos = $sentence->fetchAll(PDO::FETCH_ASSOC);
             include './views/home.php';
         }
 
-        public function create(){
-            //Logica para crear alumnos
+        public function search(){
+            if($_SERVER['REQUEST_METHOD'] == "POST"){
+                // Buscar solo alojamientos donde el título sea igual al destino
+                $titulo = $_POST['destination'];
+                if(empty($titulo)){
+                    // Si no hay título, redirigir a la vista principal
+                    $this->read();
+                    
+                    return;
+                }
+                $sentence = $this->alojamiento->search($titulo);
+                $alojaminetos = $sentence->fetchAll(PDO::FETCH_ASSOC);
+               include './views/home.php';
+                return;
+            }
 
+            if(isset($_GET['lugar'])){
+                // Buscar alojamientos por lugar
+                $lugar = $_GET['lugar'];
+                $sentence = $this->alojamiento->searchLugar($lugar);
+                $alojaminetos = $sentence->fetchAll(PDO::FETCH_ASSOC);
+                include './views/home.php';
+                return;
+            } else {
+                // Si no hay lugar, redirigir a la vista principal
+                $this->read();
+                return;
+            }
         }
+}
 
-        public function update($id){
-                //Logica para actualizar alumno
-
-        }
-
-        public function delete($id){
-                //Logica para eliminar alumno
-
-        }
-
-    }
-
-?>
+ 
